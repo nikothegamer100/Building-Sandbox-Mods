39a88e009bf6193a0ac9c804478dbd404f050fcd
nikothegamer <95710125+nikothegamer100@users.noreply.github.com>
2022-10-25 15:42:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+fetch('https://raw.githubusercontent.com/Voxelstice/niko-build-sandbox-mod-database/main/database.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+})
+
 function openTab(tab) {
     window.open(tab)
 }
